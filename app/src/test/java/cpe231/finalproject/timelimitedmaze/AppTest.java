88f548f40d0093997325a5e5c327da6ab6aa25ed
beambeambeam <supawitmarayat@gmail.com>
2025-11-21 17:45:25 +0700

    Remove unused test method from AppTest class

--- app/src/test/java/cpe231/finalproject/timelimitedmaze/AppTest.java
+++ app/src/test/java/cpe231/finalproject/timelimitedmaze/AppTest.java
@@ -1,11 +1,4 @@
 package cpe231.finalproject.timelimitedmaze;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import org.junit.jupiter.api.Test;
-
 class AppTest {
-    @Test
-    void appPrintsHelloWorld() {
-        assertEquals("Hello World", App.getGreeting());
-    }
 }
